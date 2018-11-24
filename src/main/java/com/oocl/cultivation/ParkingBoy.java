510433563fa3ea33ee71d3b2f697378247df1301
HOJO3 <joe.ho@oocl.com>
2018-11-24 21:43:07 +0800

    modify unit test

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -17,7 +17,7 @@ public class ParkingBoy {
             lastErrorMessage = null;
             return ticket;
         }else{
-            lastErrorMessage = "The parking lot is full.";
+            lastErrorMessage = "Not enough position.";
             return null;
         }
     }
